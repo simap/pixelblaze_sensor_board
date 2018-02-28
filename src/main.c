035cd377d793854522e6569fc2f81d69ebc9804f
Ben Hencke <brainstar@gmail.com>
2018-02-27 20:01:40 -0800

    compress fft buckets using exponential curve, accelerometer

--- src/main.c
+++ src/main.c
@@ -9,28 +9,56 @@
  */
 
 #include "stm32f0xx.h"
-#include "stdio.h"
 #include "stdbool.h"
+#include "string.h"
+#include "stdlib.h"
 
 #define N 512
 #define NLOG2 9
 
+#define CURVE_EXP 73216 // 1.1172 * 2^16 - chosen to go from 0-256 in 32 iterations
+
+#define LIS3DH_ADDR (0x18<<1)
+
+enum {
+	IDLE, STARTING, SENDING_REG, READING, STOPPING, NEEDSRESET
+} I2CMode = NEEDSRESET;
+
+
+extern const short Sinewave[];
 extern int fix_fft(short fr[], short fi[], short m, short inverse);
-extern uint16_t isqrt(uint32_t x);
+extern int32_t fix16_sqrt(int32_t inValue);
+
+void processBuffer(int16_t * bufferToProcess);
+void jsonKey(char ** pp, const char * key, uint32_t value, int places);
+void jsonFraction(char ** pp, uint32_t value, int places);
 
 void initRcc();
 void initTim1();
 void initAdc();
 void initGpio();
+void initUart();
 void initDma();
+void initI2C();
+
+void sendOutBuffer();
+
+void i2cReadReg(uint8_t addr, uint8_t reg, uint8_t * value, uint8_t len);
+void initAccelerometer();
+void startAccelerometerPoll();
 
 volatile uint16_t adcBuffer[7]; //updated by DMA @ 20KHz
+volatile int16_t accelerometer[3]; //updated by DMA
 volatile uint32_t ms = 0; //updated by SysTick
 
+//ping-pong buffer for audio
 volatile bool readDone;
 int readSide;
 int readPos;
-int16_t buffer[2][N]; //ping-pong buffer
+int16_t buffer[2][N];
+
+char outBuffer[512];
+int outBufferLen;
 
 int main(void) {
 	SysTick_Config(SystemCoreClock/1000); //tick interval 1ms
@@ -39,29 +67,142 @@ int main(void) {
 	initDma();
 	initAdc();
 	initGpio();
+	initUart();
+	initI2C();
 
 	for (;;) {
+
+		if (I2CMode == NEEDSRESET)
+			initAccelerometer();
+
 		//listen for message that the sample buffer is ready to process
 		if (readDone) {
 			readDone = false;
 
 			GPIO_WriteBit(GPIOB, GPIO_Pin_1, 1);
 
-			int16_t imag[N];
-			for (int i = 0; i < N; i++)
-				imag[i] = 0;
+			//start polling the accelerometer now, it can run in the background while the FFT processes
+			startAccelerometerPoll();
 
 			//grab the side we're not currently writing to and do an FFT on it
-			int16_t *buf = &buffer[!readSide][0];
-			fix_fft(buf, &imag[0], NLOG2, 0);
-			//calculate magnitude
-			for (int i = 0; i < N/2; i++)
-				imag[i] = isqrt(imag[i]*imag[i] + buf[i]*buf[i]);
+			processBuffer(&buffer[!readSide][0]);
+
+//			i2cReadReg(LIS3DH_ADDR, 0x28, (uint8_t *) &accelerometer[0], 6);
 
 			GPIO_WriteBit(GPIOB, GPIO_Pin_1, 0);
 		}
 
 	}
+}
+
+void processBuffer(int16_t * bufferToProcess) {
+	union {
+		int16_t imaginary[N]; //imaginary component
+		uint16_t magnitude[N]; //calculated magnitude
+	} tmp;
+
+	//init imaginary with zeros and calculate average (DC offset)
+	uint32_t total = 0;
+	for (int i = 0; i < N; i++) {
+		tmp.imaginary[i] = 0;
+		total += bufferToProcess[i];
+	}
+	int average = total >> NLOG2;
+
+	for (int i = 0; i < N; i++) {
+		//filter out DC (subtract the average) then
+		//apply a windowing function, borrowing Sinewave LUT from fix_fft
+		//the positive bit of Sinewave ranges from index 0-512 (9 bit positions)
+		//could probably optimize: si = i * 512 / N == (i << 9) >> 9 == i
+		int si = i * 512 / N;
+		bufferToProcess[i] -= average;
+		bufferToProcess[i] = (Sinewave[si] * bufferToProcess[i]) >> 16;
+	}
+
+	//run the FFT (runs in place, overwriting buf and imaginary)
+	fix_fft(bufferToProcess, &tmp.imaginary[0], NLOG2, 0);
+
+	//an fft of 512 gives us 256 buckets of frequency info. at 20khz, each has ~38Hz
+	//we don't really want all 256 buckets of frequency info
+	//if we compressed this linearly, we'd lose a lot of low/mid tone info
+
+	//start making output buffer
+	char * p = outBuffer;
+
+	//TODO calculate total audio energy.
+
+	//compress these on an exponential curve. TODO insert math here
+	//each pass gives new top frequency that is at least 1 bucket higher than the last pass
+	//run through and average all the buckets up to that new top
+	int k = 0;
+	int i = 0;
+	uint32_t acc = 0; // 16.16 fixed point for the curve
+	// acc = acc * 1.1172 + 1
+	for (; acc <= N/2 << 16; k++) {
+		acc = ((uint64_t)acc * (uint64_t)CURVE_EXP) >> 16;
+		acc += 0x10000;
+		//calculate magnitude of frequency buckets for this group
+		int top = acc >> 16;
+		int size = top - i;
+		uint32_t total = 0;
+		for (; i < top; i++) {
+			//using the fix16_sqrt gives us a bit more resolution as we get
+			//8 bits more using this over an integer sqrt
+			int32_t t = fix16_sqrt(
+					tmp.imaginary[i] * tmp.imaginary[i] +
+					bufferToProcess[i] * bufferToProcess[i]);
+
+			//we can't keep all those extra bits, but 4 of 8 seems like a good value
+			//as this only overloads a little and only for REALLY LOUD inputs
+			t >>= 4;
+			total += t;
+		}
+		uint32_t average = total / size;
+		//cap to 16 bit value
+		if (average > 0xffff)
+			average = 0xffff;
+
+		//write this out
+//		jsonFraction(&p, average, 5);
+	}
+
+
+
+	for (int i = 1; i < 7; i++) {
+		jsonFraction(&p, adcBuffer[i]<<4, 5);
+	}
+	
+
+	*(p - 1) = '\n'; //replace last comma
+	//NOTE: no null terminator necessary.
+
+	outBufferLen = p - outBuffer;
+	sendOutBuffer();
+}
+
+void jsonKey(char ** pp, const char * key, uint32_t value, int places) {
+	char * p = *pp;
+	*p++ = '"';
+	//copy key until null
+	while ((*p++ = *key++) != 0)
+		;
+	*(p-1) = '"'; //replace the copied null
+	*p++ = ':';
+	*pp = p;
+}
+
+void jsonFraction(char ** pp, uint32_t value, int places) {
+	char * p = *pp;
+	*p++ = '.';
+	//only write the lower 16 bits of value as a fractional up to a number of places
+	//based on https://codereview.stackexchange.com/a/109219
+	do {
+		value *= 10;
+	    *p++ = '0' + (value >> 16);
+	    value &= ((1 << 16) - 1);
+	} while (value > 0 && places--) ;
+	*p++ = ',';
+	*pp = p;
 }
 
 void initRcc() {
@@ -77,11 +218,11 @@ void initRcc() {
 }
 
 void initTim1() {
-	TIM1->CR2 |= TIM_CR2_MMS_1 | TIM_CR2_MMS_0; //compare pulse - magic trigger needed for ADC
+	TIM1->CR2 |= TIM_CR2_MMS_1 | TIM_CR2_MMS_0; //output compare pulse - magic trigger needed for ADC
 	TIM1->CR1 |= TIM_CR1_CEN | TIM_CR1_ARPE; // enable and auto-reload
 	TIM1->ARR = 2399; //48mhz / 2400 = 20khz
 
-	//Generate an update event to reload the Prescaler and the repetition countervalue immediatly
+	//Generate an update event to reload the Prescaler and the repetition countervalue immediately
 	TIM1->EGR = TIM_EGR_UG;
 }
 
@@ -111,11 +252,27 @@ void initAdc() {
 }
 
 void initGpio() {
-	//set a0, 2, 4, 5, 6, 7 and b1 to analog inputs
-	GPIOA->MODER |= GPIO_MODER_MODER0 | GPIO_MODER_MODER1 | GPIO_MODER_MODER4 |
-					GPIO_MODER_MODER5 | GPIO_MODER_MODER6 | GPIO_MODER_MODER7;
+	//select alternate function 1 (usart1) for pin 2 and 3 - on the stm23f040f4 it defaults to TIM15_CH1/TIM15_CH2
+	GPIOA->AFR[0] |= 0b0001 << 8 | 0b0001 << 12;
+	//select alternate function 4 (i2c1) for pin 9 and 10
+	GPIOA->AFR[1] |= 0b0100 << 4 | 0b0100 << 8;
+	//set a0, a1, a4, a5, a6, a7 and b1 to analog inputs
+	//set a2, a3 as alternate mode (tx, rx)
+	GPIOA->MODER |= GPIO_MODER_MODER0 | GPIO_MODER_MODER1 |
+			GPIO_MODER_MODER2_1 | GPIO_MODER_MODER3_1 | //set up a2 and a3 as alt function (usart1)
+			GPIO_MODER_MODER4 | GPIO_MODER_MODER5 | GPIO_MODER_MODER6 | GPIO_MODER_MODER7 |
+			GPIO_MODER_MODER9_1 | GPIO_MODER_MODER10_1; //set up a9 and a10 as alt function (i2c)
+
 //	GPIOB->MODER |= GPIO_MODER_MODER1;
-	GPIOB->MODER |= GPIO_MODER_MODER1_0; //output
+	GPIOB->MODER |= GPIO_MODER_MODER1_0; //output for scoping timings
+}
+
+void initUart() {
+	USART1->BRR = 0x1A1; // example from manual for 115200 @ 48mhz
+	//enable tx, and uart, leave everything else 0 for 8N1
+	USART1->CR1 = USART_CR1_TE | USART_CR1_UE;
+	//enable DMA for transmits
+	USART1->CR3 |= USART_CR3_DMAT;
 }
 
 void initDma() {
@@ -133,6 +290,31 @@ void initDma() {
 
 	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 	NVIC_SetPriority(DMA1_Channel1_IRQn, 0);
+
+	//NOTE DMA channel2 is used on the fly by sendOutBuffer()
+
+	//configure DMA to read from ADC into a buffer
+	DMA1_Channel3->CPAR = (uint32_t) (&(I2C1->RXDR)); //point dma to rx data reg
+	DMA1_Channel3->CMAR = (uint32_t) (&accelerometer[0]); //point DMA to buffer memory
+	DMA1_Channel3->CNDTR = 6; //count of transfers per circle
+	//enable DMA_CCR_CIRC (circular) mode (so addresses reset when its done)
+	//set DMA_CCR_MINC to incrememnt memory address
+	//set DMA_CCR_TCIE for transfer complete interrupt
+	DMA1_Channel3->CCR |= DMA_CCR_CIRC | DMA_CCR_MINC | DMA_CCR_TCIE;
+	DMA1_Channel3->CCR |= DMA_CCR_EN; //enable channel 1 dma
+
+	NVIC_EnableIRQ(DMA1_Channel2_3_IRQn);
+	NVIC_SetPriority(DMA1_Channel2_3_IRQn, 0);
+
+}
+
+void initI2C() {
+	I2C1->TIMINGR = (uint32_t) 0x0000020B; //magic value from cubemx for 400khz
+	//enable the module and interrupts for any error condition
+	I2C1->CR1 = I2C_CR1_PE | I2C_CR1_ERRIE | I2C_CR1_STOPIE | I2C_CR1_NACKIE | I2C_CR1_RXDMAEN;
+
+	NVIC_EnableIRQ(I2C1_IRQn);
+	NVIC_SetPriority(I2C1_IRQn, 4);
 }
 
 void SysTick_Handler(void) {
@@ -140,18 +322,169 @@ void SysTick_Handler(void) {
 	ms++;
 }
 
+//handle DMA for the channel doing ADC
 void DMA1_CH1_IRQHandler() {
 	if (DMA1->ISR & DMA_ISR_TCIF1) {
 		//the dma transfer is complete, save off the audio channel to a ping-pong buffer
-		buffer[readSide][readPos] = adcBuffer[0];
+		buffer[readSide][readPos] = adcBuffer[0] << 3;
 		readPos++;
 		if (readPos >= N) {
 			readSide = !readSide;
 			readPos = 0;
 			readDone = true;
 		}
+
 	}
 
-	DMA1->IFCR = DMA1->ISR; //is this bad? this seems right, but also wrong somehow
+	//is this bad? this seems right, but also wrong somehow
+	//unset any set bits for channel1
+	DMA1->IFCR = DMA1->ISR & 0xf;
 }
 
+void sendOutBuffer() {
+	DMA1_Channel2->CCR = 0; //disable, reset state
+	DMA1_Channel2->CPAR = (uint32_t) &USART1->TDR;
+	DMA1_Channel2->CMAR = (uint32_t) &outBuffer[0];
+	DMA1_Channel2->CNDTR = outBufferLen;
+	//set DMA_CCR_MINC to increment mem address
+	//set DMA_CCR_DIR for out to perepheral
+	//set DMA_CCR_EN to make it so
+	DMA1_Channel2->CCR = DMA_CCR_MINC | DMA_CCR_DIR | DMA_CCR_EN;
+}
+
+//write a single value to a register. Slow, blocking, but usually only used during startup
+//TODO could be more resilient to transient i2c errors. If things go really sideways this will just block forever
+void i2cWriteReg(uint8_t addr, uint8_t reg, uint8_t value) {
+	while (I2C1->ISR & I2C_ISR_BUSY)
+		;
+	//initiate start, send out slave address
+	I2C_TransferHandling(I2C1, addr, 2, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+	//wait for TXIS
+	while (I2C_GetFlagStatus(I2C1, I2C_ISR_TXIS) == 0)
+		;
+	//write out register address of interest
+	I2C_SendData(I2C1, reg);
+	while (I2C_GetFlagStatus(I2C1, I2C_ISR_TXIS) == 0)
+		;
+	I2C_SendData(I2C1, value);
+	//wait for TC
+	while (I2C_GetFlagStatus(I2C1, I2C_ISR_TC) == 0)
+		;
+	I2C_GenerateSTOP(I2C1, ENABLE);
+}
+
+void i2cReadReg(uint8_t addr, uint8_t reg, uint8_t * value, uint8_t len) {
+	while (I2C1->ISR & I2C_ISR_BUSY)
+		;
+	//initiate start, send out slave address
+	I2C_TransferHandling(I2C1, addr, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+	//wait for TXIS
+	while (I2C_GetFlagStatus(I2C1, I2C_ISR_TXIS) == 0)
+		;
+	//write out register address of interest
+	I2C_SendData(I2C1, reg | 0x80);
+	//wait for TC
+	while (I2C_GetFlagStatus(I2C1, I2C_ISR_TC) == 0)
+		;
+	I2C_TransferHandling(I2C1, LIS3DH_ADDR, len, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
+
+	for (int i = 0; i < len; i++) {
+		//wait for RXNE
+		while (I2C_GetFlagStatus(I2C1, I2C_ISR_RXNE) == 0)
+			;
+		value[i] = I2C_ReceiveData(I2C1);
+	}
+
+	while (I2C1->ISR & I2C_ISR_BUSY)
+		;
+}
+
+void initAccelerometer() {
+	I2C1->CR1 = I2C_CR1_PE | I2C_CR1_ERRIE | I2C_CR1_STOPIE | I2C_CR1_NACKIE | I2C_CR1_RXDMAEN;
+	//configure registers for desired mode
+	//set CTRL_REG1(20h) - ODR 100hz = 0101 | lpen off = 0 | xzy enable = 111
+	i2cWriteReg(LIS3DH_ADDR, 0x20, 0b01010111);
+	//set CTRL_REG4(23h) - BDU continuous = 1 | BLE big = 0 | FS 16g = 11 | HR high = 1 | ST off = 00 | SIM x = 0
+	i2cWriteReg(LIS3DH_ADDR, 0x23, 0b10111000);
+	I2CMode = IDLE;
+}
+
+//start poll sequence by initiating a start and sending the slave address
+void startAccelerometerPoll() {
+	//check to see if things are in the right state for this, or make it so
+	//maybe something went wrong, and we can reset things back to normal.
+	if (I2CMode == IDLE) {
+		I2C_TransferHandling(I2C1, LIS3DH_ADDR, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+		I2CMode = STARTING;
+		I2C1->CR1 |= I2C_CR1_TXIE; //listen for TXIS, indicating that we can start writing
+	} else {
+		I2CMode = NEEDSRESET;
+	}
+}
+
+void I2C1_ErrorHandler() {
+	I2CMode = NEEDSRESET;
+	I2C_GenerateSTOP(I2C1, ENABLE);
+}
+
+void I2C1_IRQHandler() {
+	if (I2C1->ISR & (I2C_ISR_NACKF | I2C_ISR_ARLO | I2C_ISR_BERR)) {
+		I2C1_ErrorHandler();
+	} else {
+		switch (I2CMode) {
+		case STARTING:
+			//expect I2C_ISR_TXIS
+			if (!(I2C1->ISR & I2C_ISR_TXIS)) {
+				I2C1_ErrorHandler();
+				break;
+			}
+			I2CMode = SENDING_REG;
+			I2C_SendData(I2C1, 0x28 | 0x80); //start at OUT_X_L and set MSB to increment per read
+			I2C1->CR1 &= ~I2C_CR1_TXIE; //ignore TXIS for now
+			I2C1->CR1 |= I2C_CR1_TCIE; //listen for TC
+
+			break;
+		case SENDING_REG:
+			//expect I2C_ISR_TC
+			if (!(I2C1->ISR & I2C_ISR_TC)) {
+				I2C1_ErrorHandler();
+				break;
+			}
+			I2CMode = READING;
+			I2C_TransferHandling(I2C1, LIS3DH_ADDR, 6, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
+			I2C1->CR1 &= ~I2C_CR1_TCIE; //ignore TC for now, waiting for DMA
+			break;
+		case READING:
+			//DMA is handling the reads, so if we get here there's probably an error
+			I2C1_ErrorHandler();
+			break;
+		case STOPPING:
+			//expect I2C_ISR_STOPF
+			if (!(I2C1->ISR & I2C_ISR_STOPF)) {
+				I2C1_ErrorHandler();
+				return;
+			}
+			I2CMode = IDLE;
+			break;
+		case IDLE:
+			//don't care really
+			break;
+		case NEEDSRESET:
+			//can't do much here
+			break;
+		}
+	}
+
+	I2C1->ICR = I2C1->ISR; //is this bad? this seems right, but also wrong somehow
+}
+
+//handle DMA for the channel doing I2C
+void DMA1_CH2_3_IRQHandler() {
+	if (DMA1->ISR & DMA_ISR_TCIF3) {
+		//i2c transfer complete
+		I2CMode = STOPPING;
+	}
+	//is this bad? this seems right, but also wrong somehow
+	//unset any set bits for channel3
+	DMA1->IFCR = DMA1->ISR & 0xf00;
+}
